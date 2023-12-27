2862a14db3948155e044ba108aedc0007dbe7e35
henri <henri.me@skiff.com>
2023-12-27 15:31:51 +0100

    import the new fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,10 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			"satoshi": "Satoshi-Regular",
+			"clash-display": "Clash-Display Semibold"
+		},
 		extend: {},
 	},
 	plugins: [],
